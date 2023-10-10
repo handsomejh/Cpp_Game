37e05ec187e375c7210e0d0cce9b069bd1dfc888
handsomejh <875976418@qq.com>
2023-10-10 22:19:31 +0800

    添加扫雷和贪吃蛇游戏

---
+++ 扫雷/扫雷.cpp
@@ -0,0 +1,205 @@
+﻿// 扫雷.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <iostream>
+#include<time.h>
+#include<graphics.h>
+#include<mmsystem.h>
+#pragma comment(lib,"winmm.lib")
+
+using namespace std;
+
+#define ROW 10 //定义数组的行
+#define COL 10 //定义数组的列
+#define MINE_NUM 10 //雷的数量
+#define GRID_W 40 //格子的宽度
+//定义二维数组来存放数据
+int map[ROW][COL];
+int cnt = 0;
+IMAGE img[12]; //保存所有的图片
+
+void playMusic(const char fileName[])
+{
+    char str[50] = "";
+    sprintf_s(str, "close ./resource/%s", fileName);
+    mciSendString(str, 0, 0, 0);
+    sprintf_s(str, "open ./resource/%s", fileName);
+    mciSendString(str , 0, 0, 0);
+    sprintf_s(str, "play ./resource/%s", fileName);
+    mciSendString(str, 0, 0, 0);
+
+}
+
+//数据初始化
+void gameInit()
+{
+    playMusic("start.mp3");
+    //设置随机数种子
+    srand((unsigned)time(NULL));
+    //随机在数组中生成10个-1用来表示雷
+    for (int i = 0; i < MINE_NUM;)
+    {
+        //随机生成下标
+        int r = rand() % ROW;
+        int c = rand() % COL;
+        if (map[r][c] == 0)
+        {
+            map[r][c] = -1;
+            i++;
+        }     
+    }
+    //让以-1为中心的九宫格所有的数据+1（-1除外）
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int k = 0; k < COL; k++)
+        {
+            if (map[i][k] == -1)
+            {
+                for(int r = i-1;r<=i+1;r++)
+                    for (int c = k - 1; c <= k + 1; c++)
+                    {
+                        if ((r >= 0 && r < ROW && c >= 0 && c < COL) && map[r][c] != -1)
+                            map[r][c]++;
+                    } 
+            }
+        }
+    }
+    char str[50] = "";
+    for (int i = 0; i < 12; i++)
+    {
+        sprintf_s(str, "./resource/%d.jpg", i);
+        loadimage(&img[i], str, GRID_W, GRID_W);
+    }
+
+    //数据加密   把所有的数据加20
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int k = 0; k < COL; k++)
+        {
+            map[i][k] += 20;
+        }
+
+    }
+
+}
+
+
+//界面绘制
+void gameDraw()
+{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int k = 0; k < COL; k++)
+        {
+            if (map[i][k] >= 0 && map[i][k] <= 8)
+                putimage(i* GRID_W, k* GRID_W, &img[map[i][k]]);
+            else if (map[i][k] == -1)
+            {
+                putimage(i * GRID_W, k * GRID_W, &img[9]);
+            }
+            else if (map[i][k] >= 19 && map[i][k]<=28)
+            {
+                putimage(i * GRID_W, k * GRID_W, &img[10]);
+            }
+            else if(map[i][k] > 28)
+                putimage(i * GRID_W, k * GRID_W, &img[11]);
+        }
+
+    }
+}
+
+void jude(int row, int col)
+{
+    if (map[row][col] == -1)
+    {
+        //输
+    }
+    
+    if (cnt == (ROW * ROW - MINE_NUM))
+        MessageBox(GetHWnd(), "赢了", "good", MB_OK);
+}
+
+//点开附近空白格
+void openNull(int row, int col)
+{
+    if (map[row][col] == 0)
+    {
+        for (int i = row - 1; i <= row + 1; i++)
+        {
+            for (int k = col - 1; k < col + 1; k++)
+            {
+                //如果不是雷就直接打开
+                if ((i>=0&&i<ROW&&k>=0&&k<COL)&&map[i][k] > 19)
+                {
+                    cnt++;
+                    map[i][k] -= 20;
+                    openNull(i, k);
+                }
+            }
+        }
+    }
+}
+
+//通过鼠标打开格子
+void mouseEvent()
+{
+    MOUSEMSG msg = GetMouseMsg(); //获取鼠标消息
+    //把鼠标当前点击坐标转成二维数组下标
+    int row = msg.x / GRID_W;
+    int col = msg.y / GRID_W;
+    //判断左右键
+    if (msg.uMsg == WM_LBUTTONDOWN)
+    {
+        playMusic("click.wav");
+        if (map[row][col] >= 19)
+        {
+            map[row][col] -= 20;
+            cnt++;
+            if (map[row][col] == 0)
+                playMusic("search.wav");
+            openNull(row, col);
+            jude(row, col);
+        }
+            
+    }
+    else if (msg.uMsg == WM_RBUTTONDOWN)//右键点击，标记格子
+    {
+        playMusic("rightClick.wav");
+        //没有打开才能标记
+        if (map[row][col] >= 19 && map[row][col] <= 28)
+        { 
+            map[row][col] += 20;
+        }
+        else if(map[row][col] > 28)
+            map[row][col] -= 20;
+    }
+}
+
+void show()
+{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int k = 0; k < COL; k++)
+        {
+            printf("%2d", map[i][k]);
+        }
+        cout << endl;
+    }
+}
+
+
+
+int main()
+{
+    //创建图形窗口
+    initgraph(GRID_W* ROW, GRID_W * COL);
+    gameInit();
+    
+    show();
+    while (true)
+    {
+        gameDraw();
+        mouseEvent();
+    }
+    return 0;
+}
+
---
+++ 贪吃蛇/贪吃蛇.cpp
@@ -0,0 +1,262 @@
+﻿#include<iostream>
+#include<easyx.h>
+#include<vector>
+#include<ctime>
+
+#define length 640
+#define width 480
+
+/*精灵类*/
+class Sprite
+{
+public:
+	Sprite() :Sprite(0, 0) {};
+	Sprite(int x, int y) :m_x(x), m_y(y), m_color(RED) {};
+	//绘制精灵
+	virtual void draw()
+	{
+		//设置填充颜色
+		setfillcolor(m_color);
+		//绘制矩形
+		fillrectangle(m_x, m_y, m_x + 10, m_y + 10);
+	}
+	//移动
+	void moveBy(int dx, int dy)
+	{
+		m_x += dx;
+		m_y += dy;
+	}
+	//碰撞检测
+	bool collision(const Sprite& other)
+	{
+		return m_x == other.m_x && m_y == other.m_y;
+	}
+	//撞墙检测
+	bool climbwall()
+	{
+		if (m_x == length || m_y == width|| m_x == 0 || m_y == 0)
+			std::cout << "撞墙了！" << std::endl;
+		return m_x == length || m_y == width || m_x == 0 || m_y == 0;
+	}
+protected:
+	int m_x;
+	int m_y;
+	COLORREF m_color;  //颜色
+};
+
+/*蛇类*/
+class Snake : public Sprite
+{
+public:
+	Snake():Snake(0,0) {};
+	Snake(int x, int y) :Sprite(x, y),dir(VK_RIGHT)
+	{
+		//初始化三节蛇
+		nodes.push_back(Sprite(20, 20));
+		nodes.push_back(Sprite(10, 20));
+		nodes.push_back(Sprite(0, 20));
+	};
+	void draw() override
+	{
+		for (int i = 0; i < nodes.size(); i++)
+		{
+			nodes[i].draw();
+		}
+	}
+	//蛇的身体移动
+	void bodyMove()
+	{
+		//身体跟着蛇头移动
+		for (size_t i = nodes.size()-1; i > 0; i--)
+		{
+			nodes[i] = nodes[i - 1];
+		}
+		//移动蛇头
+		switch (dir)
+		{
+		case VK_UP:
+			nodes[0].moveBy(0, -10);
+			break;
+		case VK_DOWN:
+			nodes[0].moveBy(0, 10);
+			break;
+		case VK_LEFT:
+			nodes[0].moveBy(-10, 0);
+			break;
+		case VK_RIGHT:
+			nodes[0].moveBy(10, 0);
+			break;
+		}
+	}
+	bool collision(const Sprite& other)
+	{
+		return nodes[0].collision(other);
+	}
+
+	bool climbwall()
+	{
+		return nodes[0].climbwall();
+	}
+	//蛇增加一节
+	void incrment()
+	{
+		nodes.push_back(Sprite());
+	}
+	int dir; //蛇的方向
+private:
+	std::vector<Sprite> nodes;  //蛇的所有节点
+	
+
+};
+
+/*食物*/
+class Food :public Sprite
+{
+public:
+	Food():Sprite(0,0)
+	{
+		changePos();
+	}
+	void draw()override
+	{
+		setfillcolor(m_color);
+		solidellipse(m_x, m_y, m_x + 10, m_y + 10);
+	}
+	//改变食物的坐标
+	void changePos()
+	{
+		//随机生成坐标
+		m_x = rand() % 64 * 10;
+		m_y = rand() % 48 * 10;
+	}
+};
+
+/*墙体*/
+class Wall:public Sprite
+{
+public:
+	Wall() :Wall(0, 0) {};
+	Wall(int x, int y) :Sprite(x, y) {};
+
+	void draw()override
+	{
+		//设置填充颜色
+		setfillcolor(GREEN);
+
+		//绘制上边框
+		for (int i = 0; i < length; i += 10)
+		{
+			fillrectangle(i, 0, i + 10, 10);
+		}
+		//绘制下边框
+		for (int i = 0; i < length; i += 10)
+		{
+			fillrectangle(i, width, i + 10, width + 10);
+		}
+		//绘制左边框
+		for (int i = 0; i < width; i += 10)
+		{
+			fillrectangle(0, i, 10, i + 10);
+		}
+		//绘制右边框
+		for (int i = 0; i < width; i += 10)
+		{
+			fillrectangle(length, i, length + 10, i + 10);
+		}
+	}
+};
+
+/*游戏场景*/
+class GameScene
+{
+public:
+	GameScene() {};
+	void run()
+	{
+		//双缓冲绘图
+		BeginBatchDraw();
+		snake.draw();
+		food.draw();
+		wall.draw();
+		snakeEatFood();
+		snakeHitWall();
+		EndBatchDraw();
+		
+		//移动蛇，改变蛇的坐标
+		snake.bodyMove();
+		//改变蛇的移动方向 获取键盘输入
+
+		//获取消息
+		ExMessage msg = { 0 };
+		while (peekmessage(&msg, EX_KEY))
+		{
+			onMsg(msg);
+		}
+	}
+	//响应消息：鼠标消息，键盘消息
+	void onMsg(const ExMessage& msg)
+	{
+		//如果有键盘消息（有没有键盘按下）
+		if (msg.message == WM_KEYDOWN)
+		{
+			//判断具体是哪个按键按下
+			if (msg.vkcode == VK_UP || msg.vkcode == VK_DOWN ||
+				msg.vkcode == VK_RIGHT || msg.vkcode == VK_LEFT)
+			{
+				if((snake.dir==VK_UP && msg.vkcode!=VK_DOWN)||
+					(snake.dir == VK_DOWN && msg.vkcode != VK_UP)||
+					(snake.dir == VK_LEFT && msg.vkcode != VK_RIGHT) || 
+					(snake.dir == VK_RIGHT && msg.vkcode != VK_LEFT))
+					snake.dir = msg.vkcode; //虚拟键码
+			}
+		}
+	}
+	//判断蛇能否吃到食物
+	void snakeEatFood()
+	{
+		if (snake.collision(food))//如果蛇和食物产生了碰撞
+		{
+			//蛇的节数增加
+			snake.incrment();
+
+			//食物重新产生在别的位置
+			food.changePos();
+		}
+	}
+
+	//判断蛇是否撞墙
+	void snakeHitWall()
+	{
+		if (snake.climbwall())//如果蛇撞了墙
+		{
+			//输出游戏结束
+			std::cout << "游戏结束！" << std::endl;
+
+			//清屏
+			return;
+		}
+	}
+private:
+	Snake snake;
+	Food food;
+	Wall wall;
+};
+
+int main()
+{
+	//设置随机数种子
+	srand(time(nullptr));
+	initgraph(length+10, width+10);
+	GameScene scene;
+	while (true)
+	{
+		
+		cleardevice();
+		scene.run();
+		Sleep(100);
+	}
+	
+	
+	getchar();
+	return 0;
+}
